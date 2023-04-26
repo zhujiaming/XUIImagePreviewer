583e01bb94d6ec7ef802bd69c79a808e16d480ac
jmzhu3 <jmzhu3@iflytek.com>
2023-04-26 14:54:36 +0800

    Initial Commit

---
+++ image_previewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  image_previewer
+//
+//  Created by zhujiaming on 2023/4/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
